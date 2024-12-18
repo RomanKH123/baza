4e01602ad2490ff31911090921c11dbf0bf05d4b
romann_101 <kotsuba.r@yandex.ru>
2024-12-19 01:39:02 +0300

    "v1.2"

--- server/delate.php
+++ server/delate.php
@@ -4,24 +4,24 @@ require_once 'table.php';
 $id = $_GET['id'];
 if ($_GET['tabl']== 1){
     mysqli_query($conect,"DELETE FROM postav WHERE `postav`.`IDизготовителя` = '$id'");    
-    header('Location: tabl_1.php');
+    header('Location: ../tabl_1.php');
     //SET FOREIGN_KEY_CHECKS=0; -- to disable them
     //SET FOREIGN_KEY_CHECKS=1; -- to re-enable them
 }
 if ($_GET['tabl']== 2){
     mysqli_query($conect,"DELETE FROM potreb WHERE `potreb`.`IDпотребителя` = '$id'");
-    header('Location: tabl_2.php');
+    header('Location: ../tabl_2.php');
 }
 if ($_GET['tabl']== 3){
     mysqli_query($conect,"DELETE FROM torgovly WHERE `torgovly`.`IDоперации` = '$id'");
-    header('Location: tabl_3.php');
+    header('Location: ../tabl_3.php');
 }
 if ($_GET['tabl']== 4){
     mysqli_query($conect,"DELETE FROM otkaz WHERE `otkaz`.`IDоперации` = '$id'");
-    header('Location: tabl_4.php');
+    header('Location: ../tabl_4.php');
 }
 if ($_GET['tabl']== 5){
     mysqli_query($conect,"DELETE FROM sertif WHERE `sertif`.`IDсертификата` = '$id'");
-    header('Location: tabl_5.php');
+    header('Location: ../tabl_5.php');
 }
 ?>
--- server/print_pl.php
+++ server/print_pl.php
@@ -4,12 +4,12 @@
     require_once 'table.php';
     if ($_POST['table'] == 2){
         $sql = mysqli_query($conect,"SELECT * FROM `potreb` WHERE `Названиепотребителя` = '$name'");
-        $url = "tabl_2.php";
+        $url = "../tabl_2.php";
     }else if ($_POST['table'] == 3){
         $sql = mysqli_query($conect,"SELECT * FROM `torgovly` WHERE `Дата` <= '$data'");
-        $url = "tabl_3.php";
+        $url = "../tabl_3.php";
     }else if ($_GET['table'] == 4){
         $sql = mysqli_query($conect,"SELECT * FROM `otkaz` WHERE `Дата` <= '$data'");
-        $url = "tabl_4.php";
+        $url = "../tabl_4.php";
     }
 ?>
